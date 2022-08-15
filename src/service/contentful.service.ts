425ee17e961d18bc867461451ce603443c47cb8c
Youlvis Jimenez <youlvis.jimenez@eia.edu.co>
2022-08-14 19:02:49 -0500

    serviceContentful

--- src/service/contentful.service.ts
+++ src/service/contentful.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, from, map } from "rxjs";
-import { Entry, createClient } from "contentful";
+import { from } from "rxjs";
+import { createClient } from "contentful";
 
 
 const CONFIG = {
   space: '3z392wln8jjc',
   accessToken: 'LexQP5EP23jRJ0RhN7819AVk9kzoJI_60WgGO2DTbo4',
-  contentTypeIds: {
-    champions: 'lolChampions'
-  }
+  contentTypeIds: {lolChampions:'lolChampions',},
 }
 
 @Injectable({
@@ -18,20 +16,16 @@ const CONFIG = {
 
 export class ContentfulService {
 
-  private cdaClient = createClient({
+  constructor() { }
+
+  private client = createClient({
     space: CONFIG.space,
     accessToken: CONFIG.accessToken
   });
 
-  constructor() { }
-
-  getPosts(query?: object): Observable<Entry<any>[]> {
-    return from(
-      this.cdaClient.getEntries({
-        ...Object,
-        content_type: CONFIG.contentTypeIds.champions,
-      })
-    ).pipe(map(posts=> posts.items)) 
+  getAllEntries() {
+    const promise = this.client.getEntries();
+    return from(promise);
   }
-
+  
 }
